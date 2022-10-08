df6ea976921d5134777578f2f258fa736c9c1ad3
Benjamin <vainin@users.noreply.github.com>
2022-10-08 14:46:32 -0500

    Updated Reflection

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,10 +1,11 @@
 ﻿namespace Lab3;
 
-/*Benjamin Wastart
+/* Benjamin Wastart
  * October 8, 2022
  * Description: A .Net-Maui app that connects to a database of Crossword questions. You can sort, update, create, or delete entries.
  * Bugs: None that I know of.
- * Reflection: 
+ * Reflection: Made me feel great when I was able to change the online database from the app. Not too hard of a project.
+ * Took awhile to fix errors to allow database to change.
  * 
  */
 public static class MauiProgram
