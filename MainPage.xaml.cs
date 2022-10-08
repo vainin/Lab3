1728b841cc21a766330f1aad99342d2d73290cdc
vanin <vanin@DESKTOP-7IE43IJ>
2022-10-08 14:36:25 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Lab3
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            EntriesLV.ItemsSource = MauiProgram.ibl.GetEntries();
+        }
+
+        void AddEntry(System.Object sender, System.EventArgs e)
+        {
+            String clue = clueENT.Text;
+            String answer = answerENT.Text;
+            String date = dateENT.Text;
+
+            int difficulty;
+            bool validDifficulty = int.TryParse(difficultyENT.Text, out difficulty);
+            if (validDifficulty)
+            {
+                InvalidFieldError invalidFieldError = MauiProgram.ibl.AddEntry(clue, answer, difficulty, date);
+                if(invalidFieldError != InvalidFieldError.NoError)
+                {
+                    DisplayAlert("An error has occurred while adding an entry", $"{invalidFieldError}", "OK");
+                }
+            }
+            else
+            {
+                DisplayAlert("Difficulty", $"Please enter a valid number", "OK");
+            }
+        }
+
+        void DeleteEntry(System.Object sender, System.EventArgs e)
+        {
+            Entry selectedEntry = EntriesLV.SelectedItem as Entry;
+            if(selectedEntry != null)
+            {
+                EntryDeletionError entryDeletionError = MauiProgram.ibl.DeleteEntry(selectedEntry.Id);
+                if(entryDeletionError != EntryDeletionError.NoError)
+                {
+                    DisplayAlert("An error has occurred while deleting an entry", $"{entryDeletionError}", "OK");
+                }
+            }
+
+        }
+
+        void EditEntry(System.Object sender, System.EventArgs e)
+        {
+
+            Entry selectedEntry = EntriesLV.SelectedItem as Entry;
+            if (selectedEntry != null)
+            {
+                selectedEntry.Clue = clueENT.Text;
+                selectedEntry.Answer = answerENT.Text;
+                selectedEntry.Date = dateENT.Text;
+
+
+                int difficulty;
+                bool validDifficulty = int.TryParse(difficultyENT.Text, out difficulty);
+                if (validDifficulty)
+                {
+                    selectedEntry.Difficulty = difficulty;
+                    Console.WriteLine($"Difficuilt is {selectedEntry.Difficulty}");
+                    EntryEditError entryEditError = MauiProgram.ibl.EditEntry(selectedEntry.Clue, selectedEntry.Answer, selectedEntry.Difficulty, selectedEntry.Date, selectedEntry.Id);
+                    if (entryEditError != EntryEditError.NoError)
+                    {
+                        DisplayAlert("An error has occurred while editing an entry", $"{entryEditError}", "OK");
+                    }
+                }
+            }
+
+
+        }
+
+        void SortClue(System.Object sender, System.EventArgs e)
+        {
+            EntriesLV.ItemsSource = MauiProgram.ibl.SortByClueEntries();
+        }
+
+        void SortAnswer(System.Object sender, System.EventArgs e)
+        {
+            EntriesLV.ItemsSource = MauiProgram.ibl.SortByAnswerEntries();
+        }
+
+        void EntriesLV_ItemSelected(System.Object sender, Microsoft.Maui.Controls.SelectedItemChangedEventArgs e)
+        {
+            Entry selectedEntry = e.SelectedItem as Entry;
+            clueENT.Text = selectedEntry.Clue;
+            answerENT.Text = selectedEntry.Answer;
+            difficultyENT.Text = selectedEntry.Difficulty.ToString();
+            dateENT.Text = selectedEntry.Date;
+
+        }
+
+
+
+
+    }
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab3;
+
+/*Benjamin Wastart
+ * October 8, 2022
+ * Description: A .Net-Maui app that connects to a database of Crossword questions. You can sort, update, create, or delete entries.
+ * Bugs: None that I know of.
+ * Reflection: 
+ * 
+ */
+public static class MauiProgram
+{
+    public static IBusinessLogic ibl = new BusinessLogic();
+
+    public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
+
